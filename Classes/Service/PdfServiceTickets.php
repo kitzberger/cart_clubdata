41072875bfc913b474c46b7909fe32b370e698b9
Philipp Kitzberger <coding@kitze.net>
2025-10-15 12:20:58 +0200

    Comment out var_dump

--- Classes/Service/PdfServiceTickets.php
+++ Classes/Service/PdfServiceTickets.php
@@ -226,7 +226,7 @@ class PdfServiceTickets extends \Extcode\CartPdf\Service\PdfService
      */
     protected function renderTicketBody($pdfType, $orderItem)
     {
-        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($orderItem);
+        //\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($orderItem);
         //exit;
 
         $bodyOut = '';
@@ -272,7 +272,7 @@ class PdfServiceTickets extends \Extcode\CartPdf\Service\PdfService
                         //\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($partConfig);
                         //exit;
                     }
-                \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($code);
+                //\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($code);
                 $this->pdf->write1DBarcode($code, 'EAN13', '150', $ypos, '', 18, 0.4, $style, 'N');
                 //if ($mod) $ypos = 30;
                 //  else $ypos += 80;
